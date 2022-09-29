29a29c6cffe02a8e9274de41b17f44a8efaf0a48
jonathan-crepeau <jon.crepeau@gmail.com>
2022-09-29 15:21:28 -0700

    add navigation bar with drop down menu & hyperlinks

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+const btn = document.querySelector('#nav-button');
+const list = document.querySelector('#drop-down');
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    list.classList.toggle('closed');
+    
+});
